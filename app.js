1216ac86a0e9c93fc8d45eb914f76c2a4e4b4016
Roderick Hall <rjhall@RodericudentMBP.attlocal.net>
2022-01-27 10:31:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Name:Roderick Hall")
+console.log("Career: Full Stack App and Web Developer")
+console.log("Description: I like Coding!")
+
+console.log("My Interest:*Gaming *Coding *TikTok *BingeWatching TV/Movies")
+
+console.log("My Previous Experience:*Ed Farm Pathways *Innovation Birmingham *HighSchoolDiploma *Associates")
+
+console.log("My Skills: *JavaScript *BAM: Swift *HTML *CSS")
+
+
