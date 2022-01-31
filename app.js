85b68b7add9c2d18292cf7952abfa0a457b7828d
Roderick Hall <rjhall@RodericudentMBP.attlocal.net>
2022-01-31 11:41:16 -0600

    updates

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ console.log("My Interest:*Gaming *Coding *TikTok *BingeWatching TV/Movies")
 console.log("My Previous Experience:*Ed Farm Pathways *Innovation Birmingham *HighSchoolDiploma *Associates")
 
 console.log("My Skills: *JavaScript *BAM: Swift *HTML *CSS")
+let myPastPositions = [{companyName: "Walmart Distrubtion", Title: "Non Con Order Filler", Description: "Fill the pallets with Essential tool and goods."}
+                       [{companyName: "Megamet Industries", Title: "Painter and Grinder", Description: "Prepared weather and bullet proof doors for in field installaton."}
 
-let skills = ['BAM Computer Skills','JavaScript','HTML','CSS']
+let mySkills = [{'BAM Computer Skills','JavaScript','HTML','CSS'}]
+console.log(mySkills[index]);
 
-function displaySkill(skills, BAM){
+
+function displaySkill(mySkills, BAM){
     if (BAM == true) {
         console.log(" *BAM " + mySkills);
     }
-} else if (BAM == false) {
+  else if (BAM == false) {
     console.log("*BAM" + mySkills)
-}
-
-
+};
